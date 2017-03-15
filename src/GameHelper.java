75200643fae0dc546a261cb78d6621bdd59a97e6
ehcore <ehcore79@gmail.com>
2017-03-15 16:44:30 +0400

    add Game helper

---
+++ src/GameHelper.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class GameHelper {
+
+    public BufferedReader getUserInput(){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader;
+    }
+}
