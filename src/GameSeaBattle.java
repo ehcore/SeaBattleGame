c1602f88f1d8b4345991bd11ab515edc11efecae
ehcore <ehcore79@gmail.com>
2017-03-15 16:24:40 +0400

    test commit4

--- src/GameSeaBattle.java
+++ src/GameSeaBattle.java
@@ -23,7 +23,7 @@ public class GameSeaBattle {
 
         int countShip = 0;
 
-        ShipManagement drDC[] = new ShipManagement[5];
+        ShipManagement drDC[] = new ShipManagement[4];
         for(int i = 0; i<drDC.length ; i++){
             drDC[i] = new ShipManagement();
             if(drDC[i].createShip()){countShip++;}
